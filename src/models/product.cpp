829a91f27591379d3745c47e954f84518573b782
FilippoVicini <filippo.vicini2@gmail.com>
2024-11-27 09:24:18 +0000

    import warning fix

--- src/models/product.cpp
+++ src/models/product.cpp
@@ -1,24 +1,17 @@
-#include "Product.h"
+#include "product.h"
 
 // Constructor
-Product::Product(const std::string &name, double price) : name(name), price(price) {}
+Product::Product(const std::string &name, double price)
+    : name(name), price(price) {}
 
 // Getter for name
-std::string Product::getName() const {
-  return name;
-}
+std::string Product::getName() const { return name; }
 
 // Setter for name
-void Product::setName(const std::string &newName) {
-  name = newName;
-}
+void Product::setName(const std::string &newName) { name = newName; }
 
 // Getter for price
-double Product::getPrice() const {
-  return price;
-}
+double Product::getPrice() const { return price; }
 
 // Setter for price
-void Product::setPrice(double newPrice) {
-  price = newPrice;
-}
+void Product::setPrice(double newPrice) { price = newPrice; }
